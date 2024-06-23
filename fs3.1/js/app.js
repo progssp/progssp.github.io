00ff112a9c5fe8592c291cc5575a274f50b89e34
progssp <shantanusingh922@gmail.com>
2024-06-23 16:42:59 +0530

    commit_june_23_2024_04_42_pm

--- fs3.1/js/app.js
+++ fs3.1/js/app.js
@@ -1,4 +1,7 @@
-new WOW().init();
+$(document).ready((evt) => {
+    new WOW().init();
+});
+
 
 function updateTab(tab_id){
     title = tab_id+"_title";
@@ -8,4 +11,16 @@ function updateTab(tab_id){
     $("#"+tabs_container_id+" #"+title).addClass("title_active");
     $("#"+tabs_container_id+" .tabs_content").removeClass("content_active");
     $("#"+tabs_container_id+" #"+tab).addClass("content_active");
+}
+
+function showSubForm(){
+    $("#number_sub_form").slideToggle();
+}
+
+function trigger_total(){
+    let adult = $("#adult").val();
+    let child = $("#child").val();
+    let infant = $("#infant").val();
+    let total = parseInt(adult) + parseInt(child) + parseInt(infant);
+    $("#dropdownMenuButton").val(total);
 }
